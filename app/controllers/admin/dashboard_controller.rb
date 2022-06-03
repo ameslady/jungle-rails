4f104af31f29aafbc866727fc3bcf9a3b902db0a
Amy Martin <amymariemart@gmail.com>
2022-06-02 21:39:55 -0700

    Admin dashboard complete

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: "Jungle", password: "book"
 
   def show
+    @category_count = Category.count
+    @total_inventory = Product.sum(:quantity)
   end
 end
