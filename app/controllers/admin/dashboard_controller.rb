dcb3c34592ddc1dd33c33a5ad79f03c95d841b4b
Amy Martin <amymariemart@gmail.com>
2022-06-03 14:17:17 -0700

    Update admin controllers to use enviro vars

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class Admin::DashboardController < ApplicationController
   # switch to using enviro variables
-  http_basic_authenticate_with name: "Jungle", password: "book"
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
 
   def show
     @category_count = Category.count
